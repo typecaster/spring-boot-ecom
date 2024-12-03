67bff1bce745c3704777f16ebe4822686df467f0
typecaster <nawazasif2886@gmail.com>
2024-12-04 01:42:29 +0530

    Added Product

---
+++ src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.ecommerce.project.controller;
+
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+    @Autowired
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("/admin/categories/{categoryId}/product")
+    public ResponseEntity<ProductDTO> addProduct(@RequestBody Product product,
+                                                 @PathVariable Long categoryId)
+    {
+        ProductDTO addedProduct = productService.addProduct(product,categoryId);
+        return new ResponseEntity<>(addedProduct, HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/ecommerce/project/service/PrductServiceImpl.java
@@ -0,0 +1,42 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.exceptions.MyResourceNotFoundException;
+import com.ecommerce.project.model.Category;
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.repositories.CategoryRepository;
+import com.ecommerce.project.repositories.ProductRepository;
+import lombok.Getter;
+import lombok.Setter;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Getter
+@Setter
+public class PrductServiceImpl implements ProductService{
+    @Autowired
+    private final ModelMapper modelMapper;
+    @Autowired
+    private final ProductRepository productRepository;
+    @Autowired
+    private final CategoryRepository categoryRepository;
+
+    public PrductServiceImpl(ModelMapper modelMapper, ProductRepository productRepository, CategoryRepository categoryRepository) {
+        this.modelMapper = modelMapper;
+        this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public ProductDTO addProduct(Product product, Long categoryId) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new MyResourceNotFoundException("Category", "categoryId", categoryId));
+        product.setCategory(category);
+        product.setImage("default.png");
+        product.setSpecialPrice(product.getPrice()-(product.getDiscount()*product.getPrice()*0.01));
+        Product savedProduct = productRepository.save(product);
+        return modelMapper.map(savedProduct, ProductDTO.class);
+    }
+}
