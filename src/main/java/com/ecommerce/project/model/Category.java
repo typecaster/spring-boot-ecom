586a43d078de0fd2ac581eadfdcfcde1581bef5c
typecaster <nawazasif2886@gmail.com>
2024-12-03 05:33:20 +0530

    added JPA and Hibernate

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,13 +1,19 @@
 package com.ecommerce.project.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
+@NoArgsConstructor
 public class Category {
+    @Id
     private Long categoryId;
     private String categoryName;
 }
