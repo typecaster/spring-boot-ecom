0775d613f9a89fe55c33e968e66e1a21b864027f
typecaster <nawazasif2886@gmail.com>
2024-12-05 04:30:48 +0530

    Changed auth mode from jwtToken to cookie based jwt

--- src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
+++ src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
@@ -13,11 +13,9 @@ public class UserInfoResponse {
     private String username;
     private List<String> roles;
 
-    public UserInfoResponse(Long id, String username, List<String> roles, String jwtToken) {
+    public UserInfoResponse(Long id, String username, List<String> roles) {
         this.id = id;
         this.username = username;
         this.roles = roles;
-        this.jwtToken = jwtToken;
     }
-
 }
